08b817e38eaa2463ba7a67652eef4cb58a499de1
ariel <darkun91@gmail.com>
2017-10-31 03:20:47 +0100

    [NEW] first commit, created C2 library

---
+++ README.md
@@ -0,0 +1,96 @@
+# CustomConfirm aka C2 : This is an amazin library !
+
+This library was made for people with this feeling : **window.confirm** is ungly !
+
+If you need to add a **confirm dialog** to your web site / web app that is beautiful or, at least easily themable,
+you are on the right repo !
+
+## Clean API
+
+You can use **C2** for two major use-cases :
+
+1. You want to add a confirm dialog when a link, set of links or buttons is / are clicked.
+
+```javascript
+CustomConfirm('button', function (confirmed, target_element) {
+  if (confirmed) {
+    // do something when the user confirms
+    // use the target element if you want to... or not !
+  } else {
+    // do something (or not) when he cancels
+  }
+});
+```
+
+2. You need to pragmatically show the confirm dialog whenever you want.
+
+```javascript
+CustomConfirm(function (confirmed) {
+  if (confirmed) {
+    // do something when the user confirms
+  } else {
+    // do something (or not) when he cancels
+  }
+});
+```
+
+### API options
+
+**C2** is developer-friendly, you can overide the default texts by passing options.
+
+Here's the full list of the available options :
+- `title (String) (default: 'Confirm dialog')` : To set the **Title** text
+- `body (String) (default: 'Are you sure ?')` : To set the **Body** text
+- `btn_yes (String) (default: 'confirm')` : To set the **Confirm button** text
+- `btn_no (String) (default: 'cancel')` : To set the **Cancel button** text
+- `btn_close (String) (default: 'close')` : To set the **Close button** text
+- `has_overlay (String) (default: true)` : To set whether to have an **overlay** markup or not
+- `targets (String | Element | Array | NodeList) (default: undefined)` : To set a **selector** or DOM element(s) 
+
+**Example**
+```javascript
+CustomConfirm({
+  targets: 'a',
+  title: 'A fancy website',
+  body: 'Are you sure you want to visit this link ?',
+  btn_yes: 'Oh yeah !',
+  btn_no: 'No, please cancel'
+}, function (confirmed, target_element) {
+  if (confirmed) {
+    // do something when the user confirms
+    location.href = target_element.href;
+  } else {
+    // do something (or not) when he cancels
+  }
+});
+```
+
+## Easily themable
+
+ **C2** has a simple markup with some ready-to-use classes. To customize your dialog you can use the following classes :
+- `c2_content` : For the main content box (the white box, check the default theme)
+- `c2_header` : For the header (contains the title and close markup)
+- `c2_title` : For the title
+- `c2_btn-close` : For the close button
+- `c2_body` : For the the inner content (where your confirmation message appears)
+- `c2_footer` : For the footer (contains confirm and cancel button)
+- `c2_btn-no` : For the cancel button
+- `c2_btn-yes` : For the confirm button
+- `c2_overlay` : For the overlay
+
+## Contribute
+
+I'm not a web designer so, i'd be really happy to have some more themes for the community.
+Feel free to create a theme and share it here by **sending a pull request**.
+
+## WIP
+
+I'll add some animations when i have some time !
+
+## Browser support
+IE 9+
+Chrome all versions
+Firefox all versions
+...
+
+Thanks !
---
+++ custom-confirm.js
@@ -0,0 +1,159 @@
+'use strict';
+
+(function () {
+    window.CustomConfirm = CustomConfirm;
+    window.C2 = CustomConfirm;
+
+    var template;
+    var showing = false;
+
+    function CustomConfirm(config, callback) {
+        if (! arguments.length) {
+            throw 'CustomConfirm: No arguments were passed';
+        }
+
+        if (typeof config === 'function') {
+            callback = config;
+        }
+
+        config = getFinalConfig(config);
+
+        if (config.targets) {
+            var limit = config.targets.length;
+    
+            if (limit > 0) {
+                var dialog = new Dialog(config, callback);
+    
+                for (var i = 0; i < limit; i++) {
+                    setupEvents(config.targets[i], dialog);
+                }
+            }
+        } else {
+            var dialog = new Dialog(config, callback);
+            dialog.show();
+        }
+    }
+
+    function Dialog(settings, callback) {
+        var _this = this;
+        var modal = getTemplate();
+
+        var title = modal.querySelector('.c2_title');
+        var close = modal.querySelector('.c2_btn-close');
+        var yes = modal.querySelector('.c2_btn-yes');
+        var no = modal.querySelector('.c2_btn-no');
+        var body = modal.querySelector('.c2_body');
+
+        title.innerHTML = settings.title;
+        body.innerHTML = settings.body;
+        close.innerHTML = settings.btn_close;
+        yes.innerHTML = settings.btn_yes;
+        no.innerHTML = settings.btn_no;
+
+        close.addEventListener('click', cancel);
+        no.addEventListener('click', cancel);
+        yes.addEventListener('click', confirm);
+
+        if (settings.has_overlay) {
+            var overlay = document.createElement('DIV');
+
+            modal.appendChild(overlay);
+            overlay.className = 'c2_overlay';
+            overlay.addEventListener('click', cancel);
+        }
+
+        function cancel(event) {
+            event.preventDefault();
+            _this.hide();
+
+            if (typeof callback === 'function') {
+                callback(false, _this.context);
+            }
+
+            _this.setContext(undefined);
+        }
+
+        function confirm(event) {
+            event.preventDefault();
+            _this.hide();
+
+            if (typeof callback === 'function') {
+                callback(true, _this.context);
+            }
+
+            _this.setContext(undefined);
+        }
+
+        _this.modal = modal;
+    }
+
+    Dialog.prototype.setContext = function (context) {
+        this.context = context;
+    };
+
+    Dialog.prototype.show = function () {
+        if (showing) {
+            $warn('CustomConfir: There\'s already a confirm showing');
+            return;
+        }
+        
+        showing = true;
+        document.body.appendChild(this.modal);
+    };
+
+    Dialog.prototype.hide = function () {
+        showing = false;
+        this.modal.remove();
+    };
+
+    function getFinalConfig(config) {
+        var _defaults = {
+            title: 'Confirm dialog',
+            body: 'Are you sure ?',
+            btn_yes: 'confirm',
+            btn_no: 'cancel',
+            btn_close: 'close',
+            has_overlay: true
+        };
+
+        if (typeof config === 'string') {
+            _defaults.targets = config;
+        } else if (typeof config === 'object') {
+            Object.assign(_defaults, config)
+        }
+
+        if (typeof _defaults.targets === 'string') {
+            _defaults.targets = document.querySelectorAll(_defaults.targets);
+        } else if (typeof _defaults.targets === 'object' && ! _defaults.targets.length) {
+            _defaults.targets = [_defaults.targets];
+        }
+
+        return _defaults;
+    }
+
+    function setupEvents(el, dialog) {
+        el.addEventListener('click', function (event) {
+            event.preventDefault();
+            dialog.setContext(el);
+            dialog.show();
+        }, false);
+    }
+
+    function getTemplate() {
+        if (! template) {
+            template = document.createElement('DIV');
+            template.className = 'c2';
+            template.style.zIndex = '9999';
+            template.innerHTML = '<div class="c2_content"> <header class="c2_header"> <h2 class="c2_title"></h2> <button class="c2_btn-close"></button> </header> <div class="c2_body"> </div><footer class="c2_footer"> <button class="c2_btn-no"></button> <button class="c2_btn-yes"></button> </footer>';
+        }
+
+        return template.cloneNode(true);
+    }
+
+    function $warn() {
+        if (typeof window.console === 'object' && typeof console.warn === 'function') {
+            console.warn.apply(console, arguments);
+        }
+    }
+
+})();
